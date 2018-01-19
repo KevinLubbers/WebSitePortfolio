098181d56f241872fcd1069006048ff3ce817662
KevinLubbers <34942938+KevinLubbers@users.noreply.github.com>
2018-01-19 11:09:53 -0500

    removed sticky header on mobile / tablet

--- CSGOsite/javaScript/csgoJS.js
+++ CSGOsite/javaScript/csgoJS.js
@@ -5,12 +5,21 @@ window.onscroll = function() {stickyNav()};
 
 function stickyNav() {
 	var navBar = document.getElementById("menu");
-	var navOffSet = navBar.offsetTop;	
+	var navOffSet = navBar.offsetTop;
 	
-	if (window.pageYOffset > navOffSet){
-		navBar.classList.add("stickyMenu");
+	
+	if (window.matchMedia("(max-width: 950px)").matches){
+		
 	}
-	else {
-		navBar.classList.remove("stickyMenu");
+	else{
+		
+		if (window.pageYOffset > navOffSet){
+			navBar.classList.add("stickyMenu");
+		}
+		else {
+			navBar.classList.remove("stickyMenu");
+		}
 	}
+	
+		
 }
